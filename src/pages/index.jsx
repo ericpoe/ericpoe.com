86184957e3aabb8afa7b4dcbe6f66f4514443251
Eric Poe <eric.poe@gmail.com>
2018-08-31 21:48:25 -0500

    Update index page to show latest blog posts from markdown
    OMG, working with graphql is pretty cool

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
-import { Link } from 'gatsby';
-
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div id="latestPosts">
+      <h1>Latest Posts</h1>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h2>{node.frontmatter.title} </h2>
+          <p>{node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+          <p>Read more...</p>
+        </div>
+      ))}
+    </div>
   </Layout>
 );
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "YYYY DD MMMM")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
