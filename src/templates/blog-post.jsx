88d28b70a5c6d88204b84ef5807949755f561ed7
Eric Poe <eric.poe@gmail.com>
2018-09-16 20:34:08 -0500

    Use page's title as a part of the TITLE tag

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Helmet from 'react-helmet';
 import Layout from '../components/layout';
 
 const blogPost = ({ data }) => {
   const post = data.markdownRemark;
   return (
-    <Layout>
-      <div className="leading-normal text-lg">
-        <h1>{post.frontmatter.title}</h1>
-        <p id="datePosted" className="pt-2">
-          <strong>{post.frontmatter.date}</strong>
-        </p>
-        <p id="timeToRead">
-          <strong>Time to read: </strong>
-          {post.timeToRead} {post.timeToRead > 1 ? 'minutes' : 'minute'}
-        </p>
-        <div
-          id="blogContent"
-          dangerouslySetInnerHTML={{ __html: post.html }}
-          className="leading-normal pt-4"
-        />
-      </div>
-    </Layout>
+    <React.Fragment>
+      <Helmet title={post.frontmatter.title} />
+      <Layout>
+        <div className="leading-normal text-lg">
+          <h1>{post.frontmatter.title}</h1>
+          <p id="datePosted" className="pt-2">
+            <strong>{post.frontmatter.date}</strong>
+          </p>
+          <p id="timeToRead">
+            <strong>Time to read: </strong>
+            {post.timeToRead} {post.timeToRead > 1 ? 'minutes' : 'minute'}
+          </p>
+          <div
+            id="blogContent"
+            dangerouslySetInnerHTML={{ __html: post.html }}
+            className="leading-normal pt-4"
+          />
+        </div>
+      </Layout>
+    </React.Fragment>
   );
 };
 
