162cb57c011486705955d5e23d6afe1a251fb10f
Eric Poe <eric.poe@gmail.com>
2018-09-19 22:30:36 -0500

    Create colophon with some styling

---
+++ src/components/colophon.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const Colophon = () => (
+  <div id="colophon" className="flex flex-col max-w-full">
+    <h2 className="flex justify-start my-1 sm:justify-center sm:my-2">Colophon</h2>
+    <ul id="socialLinks" className="list-reset flex justify-around">
+      <li id="github" mr-2>
+        <Link to="https://github.com/ericpoe" title="Eric Poe's github account">
+          github
+        </Link>
+      </li>
+      <li id="twitter" mr-2>
+        <Link to="https://twitter.com/eric_poe" title="Eric Poe's twitter">
+          twitter
+        </Link>
+      </li>
+      <li id="mastodon" mr-2>
+        <Link
+          to="https://mastodon.technology/@ericpoe"
+          title="Eric Poe's mastodon"
+        >
+          mastodon
+        </Link>
+      </li>
+      <li id="ravelry" mr-2>
+        <Link
+          to="https://www.ravelry.com/people/ericpoe"
+          title="Eric Poe's ravelry"
+        >
+          ravelry
+        </Link>
+      </li>
+      <li id="linkedin" mr-2>
+        <Link
+          to="https://www.linkedin.com/in/ericpoe/"
+          title="Eric Poe's LinkedIn"
+        >
+          LinkedIn
+        </Link>
+      </li>
+    </ul>
+    <p className="mt-6">
+      This site is built using{' '}
+      <Link to="https://www.gatsbyjs.org/" title="main site for gatsbyjs.org">
+        Gatsby
+      </Link>{' '}
+      and{' '}
+      <Link to="https://tailwindcss.com/" title="main site for tailwindcss.com">
+        TailwindCSS
+      </Link>
+    </p>
+  </div>
+);
+
+export default Colophon;
