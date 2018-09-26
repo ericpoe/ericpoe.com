2f7bcee68da9d22d2af7046398c8432dd8d09d7c
Eric Poe <eric.poe@gmail.com>
2018-09-26 00:46:09 -0500

    Better styling through semantic HTML

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const Header = ({ siteTitle }) => (
-  <nav className="bg-purple-darker text-5xl m-0 mv-auto py-6 px-6">
+  <nav className="bg-purple-darker text-5xl m-0 mv-auto py-4 px-6">
     <Link to="/" className="text-grey-lightest no-underline">
       {siteTitle}
     </Link>
