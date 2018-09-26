022f74bb8a7c1a1bd5763c14fb1c28bca2c92f80
Eric Poe <eric.poe@gmail.com>
2018-09-26 00:02:35 -0500

    Use better semantic layout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const Header = ({ siteTitle }) => (
-  <div className="bg-purple-darker">
-    <div className="my-0 mv-auto max-w-md py-6 px-4">
-      <div className="m-0 text-5xl">
-        <Link to="/" className="text-grey-lightest no-underline">
-          {siteTitle}
-        </Link>
-      </div>
-    </div>
-  </div>
+  <nav className="bg-purple-darker text-5xl m-0 mv-auto py-6 px-6">
+    <Link to="/" className="text-grey-lightest no-underline">
+      {siteTitle}
+    </Link>
+  </nav>
 );
 
 export default Header;
