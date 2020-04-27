0e8aa4e5169e297b371ceec1d8c981270a6b3944
Eric Poe <eric.poe@gmail.com>
2020-04-27 08:08:38 -0500

    Update react-helmet

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { graphql } from 'gatsby';
-import Helmet from 'react-helmet';
+import {graphql} from 'gatsby';
+import {Helmet} from 'react-helmet';
 import Layout from '../components/layout';
 
 const blogPost = ({ data }) => {
