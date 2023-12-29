bcb4b9e536549e2d1ac86d4d2f7c1801013843ef
Eric Poe <eric.poe@gmail.com>
2023-12-29 14:01:35 -0600

    Add OpenGraph to SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,11 @@ const path = require(`path`);
 module.exports = {
   siteMetadata: {
     title: 'Eric Poe',
+    description: 'A monument to a self-inflated ego',
+    siteName: 'Eric Poe - A monument to a self-inflated ego',
     siteUrl: 'https://www.ericpoe.com',
     twitterUsername: `@eric_poe`,
     image: '/images/largeGlassesProfile-clear.png',
-    description: 'A monument to a self-inflated ego',
   },
   trailingSlash: 'always',
   plugins: [
