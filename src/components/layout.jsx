77b4d3f0e1b6a6fa8d95d5c8c180012b6b86bb07
Eric Poe <eric.poe@gmail.com>
2023-12-29 16:01:50 -0600

    Replace deprecated StaticQuery with useStaticQuery
    This will make it easier to upgrade to Gatsby v6 when it comes out

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,41 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { graphql, StaticQuery } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import Header from './header';
 import Colophon from './colophon';
 import './css/index.css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 
 const Layout = function Layout({ children }) {
-  return (
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
-            }
-          }
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
         }
-      `}
-      render={(data) => (
-        <>
-          <Header siteTitle={data.site.siteMetadata.title} />
-          <section
-            id="children"
-            className="flex flex-col flex-1 max-w-4xl mx-auto w-full px-4 py-1 md:p-4"
-          >
-            {children}
-          </section>
-          <section
-            id="colophon"
-            className="flex flex-col flex-1 max-w-5xl mx-auto w-full px-4 py-1 md:p-4 border-t-4 border-indigo-900"
-          >
-            <Colophon />
-          </section>
-        </>
-      )}
-    />
+      }
+    }
+  `);
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <section
+        id="children"
+        className="flex flex-col flex-1 max-w-4xl mx-auto w-full px-4 py-1 md:p-4"
+      >
+        {children}
+      </section>
+      <section
+        id="colophon"
+        className="flex flex-col flex-1 max-w-5xl mx-auto w-full px-4 py-1 md:p-4 border-t-4 border-indigo-900"
+      >
+        <Colophon />
+      </section>
+    </>
   );
 };
 
