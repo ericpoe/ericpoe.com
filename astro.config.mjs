d0c443cc8483b349c5286faddb232c555c4b06e6
Eric Poe <eric.poe@gmail.com>
2025-12-07 15:54:51 -0600

    Add new figure component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
@@ -6,11 +7,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://www.ericpoe.com',
   trailingSlash: 'always',
-  integrations: [
-    react(),
-    tailwind(),
-    sitemap(),
-  ],
+  integrations: [mdx(), react(), tailwind(), sitemap()],
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
